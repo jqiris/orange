2a49ac0c9a17fb10fb0265c375da94e7041f079d
jmesyan <jieqiang1017@gmail.com>
2022-02-15 18:25:23 +0800

    world test

--- servers/world/api.go
+++ servers/world/api.go
@@ -2,12 +2,18 @@ package world
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/jqiris/kungfu/v2/discover"
 	"github.com/jqiris/kungfu/v2/logger"
 	"github.com/jqiris/orange/model"
 )
 
 func Login(c *gin.Context) model.Msg {
-	msg := model.Msg{}
+	serverList := discover.GetServerList()
+	msg := model.Msg{
+		Data: map[string]interface{}{
+			"serverList": serverList,
+		},
+	}
 	logger.Info("world login")
 	return msg
 }
